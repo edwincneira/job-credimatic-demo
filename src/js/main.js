f7996720db9fd50e648faa94eb6f69b30b52e3bf
Chivata Neira <eachivata@indracompany.com>
2022-07-28 20:10:52 -0500

    list of tramas ready without adjust

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ class Generator {
 
         this.elements = [
             {
-                element: document.getElementById("codigo-formato").value,
+                element: document.getElementById("codigo-formato"),
                 parameters: {
                     lon: 2,
                     obligatorio: true,
@@ -13,7 +13,7 @@ class Generator {
                 }    
             },
             {
-                element: document.getElementById("codigo-de-mensaje").value,
+                element: document.getElementById("codigo-de-mensaje"),
                 parameters: {
                     lon: 4,
                     obligatorio: true,
@@ -21,7 +21,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("pan").value,
+                element: document.getElementById("pan"),
                 parameters: {
                     lon: 19,
                     obligatorio: true,
@@ -29,7 +29,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-proceso").value,
+                element: document.getElementById("codigo-de-proceso"),
                 parameters: {
                     lon: 6,
                     obligatorio: true,
@@ -37,7 +37,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("importe-de-la-transaccion").value,
+                element: document.getElementById("importe-de-la-transaccion"),
                 parameters: {
                     lon: 12,
                     obligatorio: true,
@@ -45,7 +45,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("importe-en-la-moneda-de-conciliacion-con-la-red").value,
+                element: document.getElementById("importe-en-la-moneda-de-conciliacion-con-la-red"),
                 parameters: {
                     lon: 12,
                     obligatorio: false,
@@ -53,7 +53,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("fecha-y-hora-de-la-transaccion").value,
+                element: document.getElementById("fecha-y-hora-de-la-transaccion"),
                 parameters: {
                     lon: 14,
                     obligatorio: true,
@@ -61,7 +61,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("identificador-del-mensaje").value,
+                element: document.getElementById("identificador-del-mensaje"),
                 parameters: {
                     lon: 19,
                     obligatorio: true,
@@ -69,7 +69,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("fecha-de-caducidad").value,
+                element: document.getElementById("fecha-de-caducidad"),
                 parameters: {
                     lon: 4,
                     obligatorio: false,
@@ -77,7 +77,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-actividad").value,
+                element: document.getElementById("codigo-de-actividad"),
                 parameters: {
                     lon: 4,
                     obligatorio: false,
@@ -85,7 +85,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-pais").value,
+                element: document.getElementById("codigo-de-pais"),
                 parameters: {
                     lon: 3,
                     obligatorio: false,
@@ -93,7 +93,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("modo-de-entrada-de-los-datos").value,
+                element: document.getElementById("modo-de-entrada-de-los-datos"),
                 parameters: {
                     lon: 12,
                     obligatorio: true,
@@ -101,7 +101,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-actividad-original").value,
+                element: document.getElementById("codigo-de-actividad-original"),
                 parameters: {
                     lon: 4,
                     obligatorio: true,
@@ -109,7 +109,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("sesion-switch").value,
+                element: document.getElementById("sesion-switch"),
                 parameters: {
                     lon: 7,
                     obligatorio: true,
@@ -117,7 +117,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("sesion-origen").value,
+                element: document.getElementById("sesion-origen"),
                 parameters: {
                     lon: 12,
                     obligatorio: true,
@@ -125,7 +125,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("importe-de-tasas").value,
+                element: document.getElementById("importe-de-tasas"),
                 parameters: {
                     lon: 12,
                     obligatorio: false,
@@ -133,7 +133,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-moneda-de-las-tasas").value,
+                element: document.getElementById("codigo-de-moneda-de-las-tasas"),
                 parameters: {
                     lon: 3,
                     obligatorio: false,
@@ -141,7 +141,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-identificador-adquirente").value,
+                element: document.getElementById("codigo-identificador-adquirente"),
                 parameters: {
                     lon: 11,
                     obligatorio: true,
@@ -149,7 +149,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-la-red-o-elemento-switch-que-envio-el-mensaje").value,
+                element: document.getElementById("codigo-de-la-red-o-elemento-switch-que-envio-el-mensaje"),
                 parameters: {
                     lon: 11,
                     obligatorio: true,
@@ -157,7 +157,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-la-entidad-autorizadora").value,
+                element: document.getElementById("codigo-de-la-entidad-autorizadora"),
                 parameters: {
                     lon: 11,
                     obligatorio: false,
@@ -165,7 +165,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("referencia-de-la-operacion").value,
+                element: document.getElementById("referencia-de-la-operacion"),
                 parameters: {
                     lon: 12,
                     obligatorio: false,
@@ -173,7 +173,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-autorizacion").value,
+                element: document.getElementById("codigo-de-autorizacion"),
                 parameters: {
                     lon: 6,
                     obligatorio: false,
@@ -181,7 +181,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-respuesta").value,
+                element: document.getElementById("codigo-de-respuesta"),
                 parameters: {
                     lon: 3,
                     obligatorio: false,
@@ -189,7 +189,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("identificador-del-terminal").value,
+                element: document.getElementById("identificador-del-terminal"),
                 parameters: {
                     lon: 8,
                     obligatorio: false,
@@ -197,7 +197,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-identificador-del-comercio").value,
+                element: document.getElementById("codigo-identificador-del-comercio"),
                 parameters: {
                     lon: 15,
                     obligatorio: true,
@@ -205,7 +205,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("nombre-y-dirección-del-comercio").value,
+                element: document.getElementById("nombre-y-dirección-del-comercio"),
                 parameters: {
                     lon: 50,
                     obligatorio: false,
@@ -213,7 +213,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("datos-adicionales").value,
+                element: document.getElementById("datos-adicionales"),
                 parameters: {
                     lon: 538,
                     obligatorio: false,
@@ -221,7 +221,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-moneda").value,
+                element: document.getElementById("codigo-de-moneda"),
                 parameters: {
                     lon: 3,
                     obligatorio: true,
@@ -229,7 +229,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-moneda-original-a-la-transaccion").value,
+                element: document.getElementById("codigo-de-moneda-original-a-la-transaccion"),
                 parameters: {
                     lon: 3,
                     obligatorio: false,
@@ -237,7 +237,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("codigo-de-moneda-de-conciliacion").value,
+                element: document.getElementById("codigo-de-moneda-de-conciliacion"),
                 parameters: {
                     lon: 3,
                     obligatorio: false,
@@ -245,7 +245,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("datos-emv").value,
+                element: document.getElementById("datos-emv"),
                 parameters: {
                     lon: 300,
                     obligatorio: false,
@@ -253,7 +253,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("datos-de-la-operacion-original").value,
+                element: document.getElementById("datos-de-la-operacion-original"),
                 parameters: {
                     lon: 30,
                     obligatorio: false,
@@ -261,7 +261,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("datos-privados-del-origen").value,
+                element: document.getElementById("datos-privados-del-origen"),
                 parameters: {
                     lon: 40,
                     obligatorio: false,
@@ -269,7 +269,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("fecha-contable").value,
+                element: document.getElementById("fecha-contable"),
                 parameters: {
                     lon: 8,
                     obligatorio: false,
@@ -277,7 +277,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("forma-de-pago").value,
+                element: document.getElementById("forma-de-pago"),
                 parameters: {
                     lon: 2,
                     obligatorio: false,
@@ -285,7 +285,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("numero-de-referencia-de-la-linea").value,
+                element: document.getElementById("numero-de-referencia-de-la-linea"),
                 parameters: {
                     lon: 8,
                     obligatorio: false,
@@ -293,7 +293,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("impuesto").value,
+                element: document.getElementById("impuesto"),
                 parameters: {
                     lon: 12,
                     obligatorio: false,
@@ -301,7 +301,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("propina").value,
+                element: document.getElementById("propina"),
                 parameters: {
                     lon: 12,
                     obligatorio: false,
@@ -309,7 +309,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("comision").value,
+                element: document.getElementById("comision"),
                 parameters: {
                     lon: 12,
                     obligatorio: false,
@@ -317,7 +317,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("tipo-de-linea").value,
+                element: document.getElementById("tipo-de-linea"),
                 parameters: {
                     lon: 4,
                     obligatorio: false,
@@ -325,7 +325,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("mac").value,
+                element: document.getElementById("mac"),
                 parameters: {
                     lon: 8,
                     obligatorio: false,
@@ -333,7 +333,7 @@ class Generator {
                 }
             },
             {
-                element: document.getElementById("datos-token").value,
+                element: document.getElementById("datos-token"),
                 parameters: {
                     lon: 38,
                     obligatorio: false,
@@ -343,6 +343,8 @@ class Generator {
         ]
         this.containerTrama = document.getElementById("trama")
         this.btnCopy = document.getElementById("copy-trama")
+        this.btnInicioId = document.getElementById("inicio-id")
+        this.btnLimiteTransacciones = document.getElementById("limite-transacciones")
     }
 
     fill_hide(input_message, lim) {
@@ -352,14 +354,28 @@ class Generator {
 
     trama() {
         return this.elements.map( element => {
-            return (element.element.length > element.parameters.lon)
+            return (element.element.value.length > element.parameters.lon)
                    ? element.element
-                   : this.fill_hide(element.element, element.parameters.lon)
+                   : this.fill_hide(element.element.value, element.parameters.lon)
         }).join("")
     }
 
-    aleatoreo() {
+    list() {
+        let trx = []
+        for(let i=0; i < +this.btnLimiteTransacciones.value; i++){
+            trx.push(this.elements.map( element => this.aleatoreo(element.parameters.lon)).join("") + " ")
+        }
+        trx[trx.length -1] = trx[trx.length - 1].replace(" ", "")
+        return trx.join("");
+    }
 
+    aleatoreo(lim) {
+        const characters = "0123456789";
+        let out = "";
+        for (let i = 0; i < 1000; i++) {
+            out += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+        return out.substring(0, lim);
     }
     
     copy() {
@@ -372,5 +388,9 @@ class Generator {
         const message = this.trama()
         this.containerTrama.innerText = `${message}`
         console.log(message)
+    }
+
+    printList() {
+        this.containerTrama.innerText = `${this.list()}`
     }
 }
